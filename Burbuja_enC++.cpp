6d9949f6eeca4e51af9b03d2da8c632e9ee00e2d
LiuDoni2 <ld.unknown.2001@gmail.com>
2024-11-03 10:16:49 -0500

    Update Burbuja_enC++.cpp

--- Burbuja_enC++.cpp
+++ Burbuja_enC++.cpp
@@ -1,35 +1,41 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-void burbuja(int lista[], int n) {
-    for (int i = 0; i < n; i++) { 
-        for (int j = 0; j < n - i - 1; j++) { 
-            if (lista[j] > lista[j + 1]) {
-                int temp = lista[j];
-                lista[j] = lista[j + 1];
-                lista[j + 1] = temp;
-            }
-        }
-    }
+void burbuja(vector<int>& lista) {
+    for (int i = 0; i < lista.size(); i++)
+        for (int j = 0; j < lista.size() - i - 1; j++)
+            if (lista[j] > lista[j + 1])
+                swap(lista[j], lista[j + 1]); 
 }
 
 int main() {
-    int lista[] = {64, 34, 25, 12, 22, 11, 90, 17}; 
-    int n = sizeof(lista) / sizeof(lista[0]); 
+    int n;
+    cout << "¿Cuántos números quieres ingresar? ";
+    cin >> n;
+
+    if (n <= 0) {
+        cout << "Ingresa un número mayor que 0." << endl;
+        return 1;
+    }
+
+    vector<int> lista(n); 
+    for (int i = 0; i < n; i++) {
+        cout << "Ingresa el número " << (i + 1) << ": ";
+        cin >> lista[i]; 
+    }
 
     cout << "Lista original: ";
-    for (int i = 0; i < n; i++) {
-        cout << lista[i] << " "; 
-    }
+    for (int num : lista)
+        cout << num << " "; 
     cout << endl;
 
-    burbuja(lista, n); 
+    burbuja(lista); 
 
     cout << "Lista ordenada: ";
-    for (int i = 0; i < n; i++) {
-        cout << lista[i] << " "; 
-    }
+    for (int num : lista)
+        cout << num << " "; 
     cout << endl;
 
-    return 0; 
+    return 0;
 }
