edf229fd7670a2ff1c3a9bfbd780937147d55be8
LiuDoni2 <ld.unknown.2001@gmail.com>
2024-11-24 18:48:43 -0500

    Update Burbuja_enPython.py

--- Burbuja_enPython.py
+++ Burbuja_enPython.py
@@ -1,12 +1,16 @@
 def burbuja(lista):
     for i in range(len(lista)):
-        for j in range(len(lista) - 1):
+        intercambio = False  
+        for j in range(len(lista) - 1 - i):  
             if lista[j] > lista[j + 1]:
                 lista[j], lista[j + 1] = lista[j + 1], lista[j]
+                intercambio = True
+        if not intercambio:  
+            break
         print(f"Después de la pasada {i + 1}: {lista}")
 
 print("Ordenamiento Burbuja - Bubble Sort")
-n = int(input("Ingrese el numero de elementos: "))
+n = int(input("Ingrese el número de elementos: "))
 lista = [int(input(f"Ingrese el número en la posición {k + 1}: ")) for k in range(n)]
 
 print("Lista original:", lista)
